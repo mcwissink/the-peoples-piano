f1181a15fff26801202b5f436e1dbf23e81106c3
Mark Wissink <mwissink.b@gmail.com>
2018-11-30 13:55:52 -0500

    Fixed typo

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const path = required('path');
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
