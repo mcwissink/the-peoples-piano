5df9c0f7aa00cce2fff58ff596c36fe6b3bce95c
Mark Wissink <mwissink.b@gmail.com>
2018-12-02 16:12:26 -0500

    Made device selection better

--- src/scripts/components/MidiController.jsx
+++ src/scripts/components/MidiController.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import WebMidi from 'webmidi';
 import Soundfont from 'soundfont-player';
 
+const KEYBOARD_INPUT = "computer keyboard";
+
 export class MidiController extends React.Component  {
   constructor (props) {
     super(props);
     this.state = {
       loading: false,
       error: false,
-      devices: [],
+      devices: [KEYBOARD_INPUT],
+      device: "",
     };
     // Create our websocket
-    this.socket = new WebSocket(window.location.href.replace(/^http/, "ws"));
+    this.socket = new WebSocket(window.location.href.replace(/^http/, "ws"), null, 10000, 10);
     this.socket.onmessage = this.socketOnMessage;
 
   }
@@ -27,6 +30,8 @@ export class MidiController extends React.Component  {
       }
     });
 
+    // Tell the user we are loading things
+    this.setState({ loading: true });
     // Initalize the Soundfont
     Soundfont.instrument(new AudioContext(), 'electric_piano_1').then(soundfont => {
       // Set a reference to the sound font so we can call it later
@@ -36,7 +41,7 @@ export class MidiController extends React.Component  {
     });
   }
 
-  socketOnMessage = (message) => {
+  socketOnMessage = message => {
     // Make sure we have a soundfont initalized
     if (this.soundfont) {
       // Play the sound sent from the server
@@ -44,26 +49,39 @@ export class MidiController extends React.Component  {
     }
   }
 
-  handleDeviceSelect = (e) => {
-    this.setDevice(e.target.value);
-  }
+  handleDeviceSelect = e => {
+    const device = e.target.value;
+    this.setState({ device });
 
-  setDevice = (device) => {
-    // Tell the user we are loading things
-    this.setState({ loading: true });
     // Clean up an existing connections
     this.removeExistingDevice();
+    // The device could be a blank, so just don't do anything if we get that
+    if (device !== "" && device !== KEYBOARD_INPUT) {
+      this.setMidiDevice(device);
+    }
+  }
+
+  handleKeyPress = e => {
+    if (this.state.device === KEYBOARD_INPUT) {
+      this.playNote("C4")
+    }
+  }
+
+  setMidiDevice = device => {
     // Setup the input device
     this.input = WebMidi.getInputByName(device);
-    this.input.addListener("noteon", "all", e => {
-      this.soundfont.play(e.note.number);
-      // Broadcast the note to other clients
-      this.socket.send(JSON.stringify({note: e.note.number}));
-    });
+    this.input.addListener("noteon", "all", e => this.playNote(e.note.number));
 
     this.input.addListener("noteoff", "all", e => {
 
     });
+  }
+
+  playNote = note => {
+    // Play the sound locally
+    this.soundfont.play(note);
+    // Broadcast the note to other clients
+    this.socket.send(JSON.stringify({ note }));
   }
 
   removeExistingDevice() {
@@ -73,26 +91,36 @@ export class MidiController extends React.Component  {
   }
 
   updateDevices() {
-    const devices = WebMidi.inputs.map(input => input.name);
+    let devices = WebMidi.inputs.map(input => input.name);
+    if (devices.length === 0) {
+      this.setState({ device: "" });
+    } else {
+      this.setState({ device: devices[0] });
+      this.setMidiDevice(devices[0]);
+    }
+    // Add the compute keyboard as an input optoin
+    devices.push(KEYBOARD_INPUT);
     this.setState({ devices });
-    this.setDevice(devices[0]);
   }
 
   render() {
     const {
       devices,
+      device,
       loading,
       error,
     } = this.state;
     if (loading) {
       return <span>Loading...</span>;
-    } else if (error) {
-      return <span>Web MIDI is not supported by this browser (try using Chrome)</span>;
     } else {
       return (
-        <select onChange={this.handleDeviceSelect}>
-          {this.state.devices.map(device => <option key={device} value={device}>{device}</option>)}
-        </select>
+        <div onKeyPress={this.handleKeyPress}>
+          {error && <span>Web MIDI is not supported by this browser (try using Chrome)</span>}
+          <select value={device} onChange={this.handleDeviceSelect}>
+            <option value=""></option>
+            {this.state.devices.map(device => <option key={device} value={device}>{device}</option>)}
+          </select>
+        </div>
       );
     }
   }
